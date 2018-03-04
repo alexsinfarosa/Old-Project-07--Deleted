6e57ee5d9465366cef33923e51b188e3aaf55b19
alexsinfarosa <as898@cornell.edu>
2018-03-04 16:15:46 -0500

    Updates

--- src/utils.js
+++
@@ -1,9 +0,0 @@
-const stationUrl = `${
-  window.location.protocol
-}//newa2.nrcc.cornell.edu/newaUtil/stateStationList/all`;
-
-export const fetchStations = () => {
-  return fetch(stationUrl)
-    .then(res => res.json())
-    .catch(error => console.log("Failed to load stations", error));
-};
